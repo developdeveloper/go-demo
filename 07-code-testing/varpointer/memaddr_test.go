a7a117ef1aca4d94b414875b26aceda6f79c50be
Bob Wang <wangbo@yiqijiao.cn>
2020-12-25 21:52:43 +0800

    refactor unit test

--- 07-code-testing/varpointer/memaddr_test.go
+++ 07-code-testing/varpointer/memaddr_test.go
@@ -3,23 +3,22 @@ package varpointer
 import "testing"
 
 func Test_incUpdateScore(t *testing.T) {
-	t.Run("should increment score by + number", func(t *testing.T)  {
-    score := 10
-    expected := 11
-    incUpdateScore(&score, 1)
+	checkEqual := func(t *testing.T, expected, got int) {
+		t.Helper()
+		if expected != got {
+			t.Errorf("有问题, 期望 %d, 实际是 %d", expected, got)
+		}
+	}
 
-    if score != expected {
-      t.Errorf("有问题, 期望 %d, 实际是 %d", expected, score)
-    }
-  })
+	t.Run("should increment score by + number", func(t *testing.T) {
+		score := 10
+		incUpdateScore(&score, 1)
+		checkEqual(t, 11, score)
+	})
 
-  t.Run("should increment score by - number", func(t *testing.T)  {
-    score := 10
-    expected := 9
-    incUpdateScore(&score, -1)
-
-    if score != expected {
-      t.Errorf("有问题, 期望 %d, 实际是 %d", expected, score)
-    }
-  })
+	t.Run("should increment score by - number", func(t *testing.T) {
+		score := 10
+		incUpdateScore(&score, -1)
+		checkEqual(t, 9, score)
+	})
 }
