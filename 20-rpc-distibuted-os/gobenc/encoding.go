5b770604e782d6d35b3b300cbd81239838ee392c
Bob Wang <wangbo@yiqijiao.cn>
2021-01-14 10:29:28 +0800

    gob encoding and decoding

---
+++ 20-rpc-distibuted-os/gobenc/encoding.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/gob"
+	"fmt"
+	"os"
+	"reflect"
+)
+
+type person struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	p1 := person{"zhangsan", 20}
+	enc1 := gob.NewEncoder(os.Stdout)
+	enc1.Encode(p1)
+
+	file1, _ := os.OpenFile("/tmp/person.gob", os.O_CREATE|os.O_WRONLY, 0644)
+	enc2 := gob.NewEncoder(file1)
+	enc2.Encode(p1)
+	file1.Close()
+
+	var p2 person
+	file2, _ := os.Open("/tmp/person.gob")
+	defer file2.Close()
+	dec := gob.NewDecoder(file2)
+	dec.Decode(&p2)
+
+	fmt.Println(p2)
+	if reflect.DeepEqual(p1, p2) {
+		fmt.Println("decode ok")
+	}
+}
