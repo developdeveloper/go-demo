2d432eac0876c7e33efe6ff32fbbb43ec44ad1c3
Bob Wang <wangbo@yiqijiao.cn>
2021-01-20 10:46:13 +0800

    build with ldlflags -X

--- 25-build-with-meta/main.go
+++ 25-build-with-meta/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"25-build-with-meta/meta/config"
+	"flag"
+	"fmt"
+)
 
+var version bool
+
+func main() {
+	flag.BoolVar(&version, "version", false, "print version info")
+	flag.Parse()
+
+	if version {
+		fmt.Printf("version:\t\t%s\n", config.Version)
+		fmt.Printf("git hash:\t\t%s\n", config.GitHash)
+		fmt.Printf("build time:\t\t%s\n", config.BuildTime)
+	}
 }
---
+++ 25-build-with-meta/meta/config/config.go
@@ -0,0 +1,7 @@
+package config
+
+var (
+	Version   = ""
+	GitHash   = ""
+	BuildTime = ""
+)
